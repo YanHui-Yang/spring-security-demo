271e1530070c341f0ecffc26847e71c23f0fb57c
YanHui-Yang <124481952@qq.com>
2023-07-15 23:28:26 +0800

    feat: 新增登录后获取用户名

--- 00-memory-auth/src/main/java/com/max/mem/config/SecurityConfig.java
+++ 00-memory-auth/src/main/java/com/max/mem/config/SecurityConfig.java
@@ -18,9 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // 用户名: admin, 密码: 123456, 角色: ADMIN
-        //auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN");
         // 用户名: user, 密码: 123123, 角色: USER
-        //auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("123123")).roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("123123")).roles("USER");
         // 如果不想加密，可以在密码前定义{noop}
         // 注意，密码不加密，不能够声明PasswordEncoder Bean，把下面的PasswordEncoder Bean注释掉才能使用
         auth.inMemoryAuthentication().withUser("testNoop").password("{noop}123").roles("USER");
